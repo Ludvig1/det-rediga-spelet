c9b2da369d2ee157bec5506e6157a4c69cfd60a9
CapFozzE <ludvigaskander@live.se>
2020-03-19 14:21:20 +0100

    kmkm

---
+++ Geomitry dash/Assets/Camerafollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Camerafollow : MonoBehaviour {
+
+    public Transform Target;
+
+    public float smoothspeed = 0.125f;
+    public Vector3 offset;
+
+    void LateUpdate ()
+    {
+        transform.position = Target.position + offset; 
+    }
+
+}
